9f7de8f4def77e5b64a8c1872d2b9325219ffb2f
Mark Lim <mark.ljr2@gmail.com>
2023-03-18 14:24:12 +0800

    validation for exhibition create and update working

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+mongoose.set("runValidators", true); //https://stackoverflow.com/questions/15627967/why-mongoose-doesnt-validate-on-update contribution by Muho
+
 mongoose.connect(process.env.DATABASE_URL);
 
 // shortcut var to mongoose.connection object
--- controllers/exhibitions.js
+++ controllers/exhibitions.js
@@ -9,7 +9,6 @@ function createNew(req, res) {
 const create = async (req, res) => {
   try {
     const exhibition = new Exhibition(req.body);
-
     const newExhibition = await exhibition.save();
     console.log(`newExhibition: ${newExhibition}`);
     res.redirect("/exhibitions");
@@ -33,9 +32,7 @@ function index(req, res) {
       .populate("artworks")
       .exec()
       .then((exhibitions) => {
-        // console.log("THIS IS EXHIBITION" + exhibitions);
         Artwork.find({}).then((artworks) => {
-          // console.log("THIS IS ARTWORKS", artworks);
           res.render("exhibitions/index", { exhibitions, artworks });
         });
       });
@@ -58,18 +55,26 @@ function editPage(req, res) {
   }
 }
 
-function update(req, res) {
+const update = async (req, res) => {
   const { id } = req.params;
   try {
-    Exhibition.findByIdAndUpdate(id, req.body, { new: true })
-      .exec()
-      .then((exhibitions) => {
-        res.redirect("/");
-      });
-  } catch (error) {
-    res.send(error);
+    const updateExhibition = await Exhibition.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.redirect("/exhibitions");
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      const errors = Object.values(err.errors).map((e) => e.message);
+      console.log(`Data Model Errors: ${errors}`);
+      res.send(
+        `Unable to submit form, please refer to error message -> ${errors}`
+      );
+    } else {
+      console.log(err);
+      res.send(`${errors}`);
+    }
   }
-}
+};
 
 function del(req, res) {
   const { id } = req.params;
